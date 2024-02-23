0b8a0a12be3554419fe664c61ac8bd9cdd6e462a
David Zapata <dzr794@gmail.com>
2024-02-22 19:54:40 -0500

    se finalizo el primer componente como archivo separado

---
+++ src/HelloWorldApp.jsx
@@ -0,0 +1,11 @@
+// export function App() {
+//   return (<h1>Holiwa !!!</h1>);
+// }
+
+// * usando el snipet "rafc"
+
+export const HelloWorldApp = () => {
+  return (
+    <h1>HelloWorld App</h1>
+  )
+}
